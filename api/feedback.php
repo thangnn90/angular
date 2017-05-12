7e921ba506087a02b4e0ab63aae51d3d964d00d6
thangnn <thangnn@ominext.com>
2017-05-12 14:27:19 -0700

    add and modify

--- api/feedback.php
+++ api/feedback.php
@@ -5,3 +5,26 @@
  * Date: 12/05/2017
  * Time: 14:02
  */
+
+$serverDB = "46.51.242.216";
+$userDB = "hotline";
+$passDB = "1qazxsw2";
+$DBname = "hotline-lp";
+
+$conn = mysqli_connect($serverDB, $userDB, $passDB, $DBname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "select * from feedback";
+$data = array();
+$result = mysqli_query($sql);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+} else {
+    echo "0 result";
+}
+echo json_encode($data);
+mysqli_close($conn);
--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
 /**
  * Created by nguye on 12/05/2017.
  */
+var controllerApp = angular.nodule('controller', []);
+controllerApp.controller('controller', function ($scope, $http) {
+    $http.get('feedback.php').success(function (data) {
+        console.log("data " + data);
+        $scope.feedbacks = data;
+    });
+});
